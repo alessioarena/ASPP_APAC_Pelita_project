3c3cf88194650d5811cec717baeabf4905ef5b36
Natalie Tan <natalieytan@gmail.com>
2019-01-26 00:49:31 +1100

    Modify attack to choose food away from enemy

--- group0.py
+++ group0.py
@@ -184,7 +184,6 @@ def move_attack(bot, state, was_recur = False):
     for enemy in bot.enemy:
         if not enemy.is_noisy:
             bot.say("Go away.")
-            print("ERE")
             food_dist = [nx.shortest_path_length(state.nx_G, source = bot.position, target=i) for i in bot.enemy[0].food]
             enemy_dist = [nx.shortest_path_length(state.nx_G, source = enemy.position, target=i) for i in bot.enemy[0].food]
             food_enemy_diff = np.array(enemy_dist) - np.array(food_dist)
